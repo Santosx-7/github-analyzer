06c67e5ffa9eda12523341342060a7c450a23a32
Daniel <dsantos.fernandess@gmail.com>
2026-06-13 23:18:01 -0300

    feat: projeto inicial com Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Historico from './Historico'
+import Main from './Main'
+
+function App(){
+  return(
+    <main id="app">
+      <Historico />
+      <Main />
+    </main>
+  )
+}
+
+export default App
