e4bb280a0e75bb0ed57fa8e8dee08aae530ed4fe
Daniel <dsantos.fernandess@gmail.com>
2026-06-15 16:44:36 -0300

    Feat: Função de clic funcionando

--- src/Analyzer.jsx
+++ src/Analyzer.jsx
@@ -1,7 +1,18 @@
-function Analyzer(){
-    return(
-        <div><p>Analyzer</p></div>
-    )
-}
+import { useState } from "react";
 
-export default Analyzer
+export default function Analyzer() {
+  const [url, setUrl] = useState("");
+  function handleClick(){
+    console.log(url)
+  }
+  return (
+    <main>
+      <div>
+        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)}/>
+      </div>
+      <div>
+        <button onClick={handleClick}>Analisar</button>
+      </div>
+    </main>
+  );
+}
